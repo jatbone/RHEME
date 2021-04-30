d51d62da3996af969965e47eb249f55414c73e1b
Jakub TBone <jakub.tbone@gmail.com>
2021-04-30 12:45:03 +0200

    testing

--- src/__tests__/App.test.ts
+++ src/__tests__/App.test.ts
@@ -1,6 +1,32 @@
-describe('Test', () => {
-  it('should work', () => {
-    const n = 4
-    expect(n).toBe(4)
+import type { ThemeConfig } from '../types'
+import { r } from '../makeThemeReactive'
+
+const themeConfigMock: ThemeConfig = {
+  colors: {
+    primary: 'red',
+  },
+  button: {
+    background: r('colors.primary'),
+  },
+}
+
+describe('Testing theme config', () => {
+  it('It should have primary color', () => {
+    expect(themeConfigMock.colors.primary).toEqual('red')
+  })
+  it('It should have button', () => {
+    expect(themeConfigMock.button).toBeDefined()
+    expect(themeConfigMock.button.background).toBeDefined()
+  })
+  it('Button background prop should be function', () => {
+    const f = jest.fn(themeConfigMock.button.background)
+    f({})
+    expect(f).toBeCalledTimes(1)
+    expect(f.mock.results[0].value).toBeDefined()
+    expect(f.mock.results[0].value).toMatchObject({
+      cb: undefined,
+      refPath: 'colors.primary',
+      value: null,
+    })
   })
 })
